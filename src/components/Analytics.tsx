9e993b0a9f4d585e6e64d64a6e09ea59d07bf0f0
thiago <almeida.thiagoalvesde@gmail.com>
2024-02-20 15:11:46 -0300

    implementacao do analytics

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,16 @@
+export default function Analytics() {
+  return (
+    <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZNR1VQD92H"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-ZNR1VQD92H');
+          `
+        }} />
+    </>
+  )
+}
